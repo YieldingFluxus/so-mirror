98f678f7fe45ef62508c5d54a4d8ff6e97750901
Yielding <90570076+YieldingExploiter@users.noreply.github.com>
2022-11-29 14:04:29 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-echo '<!DOCTYPE html><html><head><meta http-equiv="Refresh" content="0; url=`cat so.txt`" />' > index.html
+SOFILE=$(cat so.txt)
+echo '<!DOCTYPE html><html><head><meta http-equiv="Refresh" content="0; url=${SOFILE}" />' > index.html
 cat _opengraph.html >> index.html
-echo '</head><body>SO -&gt; <a href="`cat so.txt`">`cat so.txt`</a><br/>Last updated at -&gt; `date`</body></html>' >> index.html
+echo '</head><body>SO -&gt; <a href="`cat so.txt`">${SOFILE}</a><br/>Last updated at -&gt; $(date)</body></html>' >> index.html
