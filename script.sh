16a5d8564694aa45e7ec7af34dca89d277c461a6
Yielding <90570076+YieldingExploiter@users.noreply.github.com>
2022-11-29 16:58:43 +0100

    fix: remove /new

--- script.sh
+++ script.sh
@@ -2,4 +2,4 @@
 SOFILE=$(cat so.txt)
 echo -e "<!DOCTYPE html><html><head><meta http-equiv=\"Refresh\" content=\"0; url=${SOFILE}\" />" > index.html
 cat _opengraph.html >> index.html
-echo -e "</head><body>SO -&gt; <a href=\"`cat so.txt`\">${SOFILE}</a><br/>Last updated at -&gt; $(date)<br/>-&gt; <a href=\"https://fuuuux🤯.astolfo.gay/new/android/legacy\">Back to Legacy Download</a></body></html>" >> index.html
+echo -e "</head><body>SO -&gt; <a href=\"`cat so.txt`\">${SOFILE}</a><br/>Last updated at -&gt; $(date)<br/>-&gt; <a href=\"https://fuuuux🤯.astolfo.gay/android/legacy\">Back to Legacy Download</a></body></html>" >> index.html
