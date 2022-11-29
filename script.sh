394fe4c9ae235b8d11cfe39594c3ffd84d165ad2
Yielding <90570076+YieldingExploiter@users.noreply.github.com>
2022-11-29 17:39:47 +0100

    fix: mention last fetch date

--- script.sh
+++ script.sh
@@ -2,4 +2,4 @@
 SOFILE=$(cat so.txt)
 echo -e "<!DOCTYPE html><html><head><meta http-equiv=\"Refresh\" content=\"0; url=${SOFILE}\" />" > index.html
 cat _opengraph.html >> index.html
-echo -e "</head><body>SO -&gt; <a href=\"${SOFILE}\">${SOFILE}</a> (<a href=\"https://yieldingfluxus.github.io/so-mirror/libFluxusAndroid.so\">Constant Mirror Link</a>)<br/>Last updated at -&gt; $(date)<br/>Back -&gt; <a href=\"https://fuuuux🤯.astolfo.gay/android/legacy\">Legacy Download</a></body></html>" >> index.html
+echo -e "</head><body>SO -&gt; <a href=\"${SOFILE}\">${SOFILE}</a> (<a href=\"https://yieldingfluxus.github.io/so-mirror/libFluxusAndroid.so\">Constant Mirror Link</a>)<br/>Last fetched at -&gt; $(date)<br/>Back -&gt; <a href=\"https://fuuuux🤯.astolfo.gay/android/legacy\">Legacy Download</a></body></html>" >> index.html
